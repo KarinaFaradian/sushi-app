63e5b853614f59164d819a2214c70644e9b3fe0c
Карина Фарадян <faradiankarina@gmail.com>
2023-02-06 18:50:25 +0400

    feat: added createAsyncThunk for getting sushi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Categories from '../components/Categories';
@@ -10,14 +9,15 @@ import SushiBlock from '../components/SushiBlock';
 import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
 import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { fetchSushi } from '../redux/slices/sushiSlice';
+import NotFound from './NotFound';
 
 const Home = () => {
   const dispatch = useDispatch();
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
+  const { items, status } = useSelector((state) => state.sushi);
 
   const { searchValue } = React.useContext(SearchContext);
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
@@ -27,45 +27,22 @@ const Home = () => {
     dispatch(setCurrentPage(page));
   };
 
-  const fetchSushi = async () => {
-    setIsLoading(true);
-
+  const getSushi = async () => {
     const search = searchValue ? `&search=${searchValue}` : '';
 
-    try {
-      const res = await axios.get(
-        `https://63c8592d5c0760f69aca662f.mockapi.io/Items?page=${currentPage}&limit=6&${
-          categoryId > 0 ? `category=${categoryId}` : ''
-        }&sortBy=${sort.sortProperty}&order=desc${search}`,
-      );
-      setItems(res.data);
-      setIsLoading(false);
-    } catch (error) {
-      console.log('error', error);
-      alert('Error')
-    } finally {
-      setIsLoading(false);
-    }
+    dispatch(
+      fetchSushi({
+        search,
+        categoryId,
+        currentPage,
+        sort,
+      }),
+    );
   };
 
   React.useEffect(() => {
-    fetchSushi();
+    getSushi();
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
-
-  // React.useEffect(() => {
-  //   setIsLoading(true);
-  //   const search = searchValue ? `&search=${searchValue}` : '';
-
-  //   axios.get(`https://63c8592d5c0760f69aca662f.mockapi.io/Items?page=${currentPage}&limit=6&${
-  //     categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${
-  //     sort.sortProperty}&order=desc${search}`)
-  //     .then(((res) => {
-  //       setItems(res.data);
-  //       setIsLoading(false);
-  //     }))
-
-  //       window.scrollTo(0, 0);
-  //       }, [categoryId, sort.sortProperty, searchValue, currentPage])
 
   const sushis = items.map((obj) => <SushiBlock key={obj.id} {...obj} />);
 
@@ -78,7 +55,11 @@ const Home = () => {
         <Sort />
       </div>
       <h2 className="content__title">All products</h2>
-      <div className="content__items">{isLoading ? skeletons : sushis}</div>
+      {status === 'error' ? (
+        <NotFound />
+      ) : (
+        <div className="content__items">{status === 'loading' ? skeletons : sushis}</div>
+      )}
       <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
---
+++ src/redux/slices/sushiSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchSushi = createAsyncThunk('sushi/fetchSushiStatus', async (params) => {
+  const { search, categoryId, currentPage, sort } = params;
+  const { data } = await axios.get(
+    `https://63c8592d5c0760f69aca662f.mockapi.io/Items?page=${currentPage}&limit=6&${
+      categoryId > 0 ? `category=${categoryId}` : ''
+    }&sortBy=${sort.sortProperty}&order=desc${search}`,
+  );
+  return data;
+});
+
+const initialState = {
+  items: [],
+  status: 'loading'
+};
+
+const sushiSlice = createSlice({
+  name: 'sushi',
+  initialState,
+  reducers: {
+    setItems: (state, action) => {
+      state.items = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchSushi.pending]: (state) => {
+      state.status = 'loadiing';
+      state.items = [];
+    },
+    [fetchSushi.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.status = 'succes';
+    },
+    [fetchSushi.rejected]: (state) => {
+      state.status = 'error';
+      state.items = [];
+    }
+  }
+});
+
+export const { setItems } = sushiSlice.actions;
+
+export default sushiSlice.reducer;
