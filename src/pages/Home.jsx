e8a0ab99d37deca4d3e10a6e424923349dcb736b
Карина Фарадян <faradiankarina@gmail.com>
2023-02-06 17:10:15 +0400

    feat: add promise

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import axios from 'axios'; 
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Categories from '../components/Categories';
@@ -15,53 +15,73 @@ const Home = () => {
   const dispatch = useDispatch();
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
 
-    const {searchValue} = React.useContext(SearchContext);
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
+  const { searchValue } = React.useContext(SearchContext);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
-    const onChangeCategory = (id) => {
-      dispatch(setCategoryId(id));
-    }
+  const onChangeCategory = (id) => {
+    dispatch(setCategoryId(id));
+  };
 
-    const onChangePage = (page) => {
-      dispatch(setCurrentPage(page));
-    }
+  const onChangePage = (page) => {
+    dispatch(setCurrentPage(page));
+  };
 
-  React.useEffect(() => {
+  const fetchSushi = async () => {
     setIsLoading(true);
+
     const search = searchValue ? `&search=${searchValue}` : '';
 
-    axios.get(`https://63c8592d5c0760f69aca662f.mockapi.io/Items?page=${currentPage}&limit=6&${
-      categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${
-      sort.sortProperty}&order=desc${search}`)
-      .then(((res) => {
-        setItems(res.data);
-        setIsLoading(false);
-      }))
+    try {
+      const res = await axios.get(
+        `https://63c8592d5c0760f69aca662f.mockapi.io/Items?page=${currentPage}&limit=6&${
+          categoryId > 0 ? `category=${categoryId}` : ''
+        }&sortBy=${sort.sortProperty}&order=desc${search}`,
+      );
+      setItems(res.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log('error', error);
+      alert('Error')
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-        window.scrollTo(0, 0);
-        }, [categoryId, sort.sortProperty, searchValue, currentPage]) 
+  React.useEffect(() => {
+    fetchSushi();
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
-    const sushis = items.map((obj) => (
-      <SushiBlock key={obj.id} {...obj} />
-  ));
+  // React.useEffect(() => {
+  //   setIsLoading(true);
+  //   const search = searchValue ? `&search=${searchValue}` : '';
 
+  //   axios.get(`https://63c8592d5c0760f69aca662f.mockapi.io/Items?page=${currentPage}&limit=6&${
+  //     categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${
+  //     sort.sortProperty}&order=desc${search}`)
+  //     .then(((res) => {
+  //       setItems(res.data);
+  //       setIsLoading(false);
+  //     }))
+
+  //       window.scrollTo(0, 0);
+  //       }, [categoryId, sort.sortProperty, searchValue, currentPage])
+
+  const sushis = items.map((obj) => <SushiBlock key={obj.id} {...obj} />);
 
   const skeletons = [...new Array(9)].map((_, i) => <Skeleton key={i} />);
 
   return (
     <div className="container">
-        <div className="content__top">
-          <Categories value={categoryId} onClickCategory={onChangeCategory} />
-          <Sort />
-        </div>
-        <h2 className="content__title">All products</h2>
-        <div className="content__items">
-          { isLoading ? skeletons : sushis }
-        </div>
-        <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+      <div className="content__top">
+        <Categories value={categoryId} onClickCategory={onChangeCategory} />
+        <Sort />
+      </div>
+      <h2 className="content__title">All products</h2>
+      <div className="content__items">{isLoading ? skeletons : sushis}</div>
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
