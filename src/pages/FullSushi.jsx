3a86559301ad18f1e0c4e7f2df3e0e2772174b2a
Карина Фарадян <faradiankarina@gmail.com>
2023-02-09 16:19:20 +0400

    feat: added useNavigate for errors in FullSushi pages

--- src/pages/FullSushi.jsx
+++ src/pages/FullSushi.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
 const FullSushi = () => {
   const [sushi, setSushi] = React.useState();
-
+  const navigate = useNavigate();
   const { id } = useParams();
 
   React.useEffect(() => {
@@ -13,7 +13,8 @@ const FullSushi = () => {
         const { data } = await axios.get('https://63c8592d5c0760f69aca662f.mockapi.io/Items/' + id);
         setSushi(data);
       } catch (error) {
-        console.log(error);
+        alert('Something is wrong');
+        navigate('/');
       }
     }
     fetchSushi();
@@ -29,26 +30,25 @@ const FullSushi = () => {
       <h3 className="sushi-block__title">{sushi.title}</h3>
       <p className="sushi-block__composition">{sushi.composition}</p>
       <h4 className="sushi-block__price">{sushi.price} $</h4>
-      <div 
-      className="cart__bottom-buttons">
-        <Link to="/"> 
-        <button className="button button--outline button--add go-back-btn" >
-          <svg
-            width="8"
-            height="14"
-            viewBox="0 0 8 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M7 13L1 6.93015L6.86175 1"
-              stroke="#D3D3D3"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+      <div className="cart__bottom-buttons">
+        <Link to="/">
+          <button className="button button--outline button--add go-back-btn">
+            <svg
+              width="8"
+              height="14"
+              viewBox="0 0 8 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 13L1 6.93015L6.86175 1"
+                stroke="#D3D3D3"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
 
-          <span>Come back</span>
+            <span>Come back</span>
           </button>
         </Link>
       </div>
